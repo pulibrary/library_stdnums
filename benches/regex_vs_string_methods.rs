b11e69992a43026a780217079bd1787372daacbe
Jane Sandberg <js7389@princeton.edu>
2024-12-24 09:21:46 -0800

    Run cargo fmt

--- benches/regex_vs_string_methods.rs
+++ benches/regex_vs_string_methods.rs
@@ -1,29 +1,30 @@
-use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use library_stdnums::lccn::valid;
 
 fn valid_benchmark(c: &mut Criterion) {
-  let lccns = [
-    "78-890351",
-    "n78-890351",
-    "2001-890351",
-    "nb78-890351",
-    "agr78-890351",
-    "n2001-890351",
-    "nb2001-890351",
-    "n78-89035100444",
-    "n78",
-    "na078-890351",
-    "n078-890351",
-    "0an78-890351",
-    "n78-89c0351"
-  ];
+    let lccns = [
+        "78-890351",
+        "n78-890351",
+        "2001-890351",
+        "nb78-890351",
+        "agr78-890351",
+        "n2001-890351",
+        "nb2001-890351",
+        "n78-89035100444",
+        "n78",
+        "na078-890351",
+        "n078-890351",
+        "0an78-890351",
+        "n78-89c0351",
+    ];
 
-  let mut group = c.benchmark_group("valid");
-  for i in lccns.iter() {
-    group.bench_with_input(BenchmarkId::new("char", i), i,
-      |b, i| b.iter(|| valid(*i, false)));
-  }
-  group.finish();
+    let mut group = c.benchmark_group("valid");
+    for i in lccns.iter() {
+        group.bench_with_input(BenchmarkId::new("char", i), i, |b, i| {
+            b.iter(|| valid(*i, false))
+        });
+    }
+    group.finish();
 }
 
 criterion_group!(valid_benches, valid_benchmark);
