74a94bbef520b05ba6a7686abce425233ed480bc
Christina Chortaria <actspatial@gmail.com>
2025-03-07 13:32:03 -0500

    Update issn.rs to fix panic in issn when checkdigit is 0 Calculate checkdigit for isbn 10
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>
    Co-authored-by: Kevin Reiss <kevinreiss@users.noreply.github.com>
    Co-authored-by: Mark Zelesky <mzelesky@users.noreply.github.com>
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>
    Co-authored-by: regineheberlein <regineheberlein@users.noreply.github.com>
    Co-authored-by: Ryan Laddusaw <rladdusaw@users.noreply.github.com>

--- src/isbn.rs
+++ src/isbn.rs
@@ -8,7 +8,24 @@ pub fn checkdigit(isbn: &str) -> Option<char> {
 }
 
 fn checkdigit_ten(isbn: &str) -> char {
-  'a'
+  let clean_string = isbn.replace("-", "");
+  let first_nine = clean_string.chars().take(9);
+  let first_nine_digits = first_nine.filter_map(|x| x.to_digit(10));
+  let multiplied = first_nine_digits.enumerate().map(|(index, digit)| digit * (10 - index as u32));
+
+  let summed: u32 = multiplied.sum();
+  let modulus: u32 = summed % 11;
+  from_digit_to_checkdigit(modulus)
+}
+
+fn from_digit_to_checkdigit(num: u32) -> char {
+  println!("num is: {}", num);
+    let orig_num = char::from_digit((11_u32 - num) % 11, 11).unwrap();
+    if orig_num == 'a' {
+        'X'
+    } else {
+        orig_num
+    }
 }
 
 fn checkdigit_thirteen(isbn: &str) -> char {
